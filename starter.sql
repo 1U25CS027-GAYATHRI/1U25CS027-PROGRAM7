a7fd3ae240130fb2b426978f9ae3490f8a7f15d7
1U25CS027-GAYATHRI <gayubalu2007@gmail.com>
2026-08-31 11:32:39 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -12,5 +12,48 @@ insert marksheet values("1","Arun","CSE","85"),
   ("3","Karthik","CSE","92"),
   ("4","Nisha","ECE","67"),
   ("5","Rahul","IT","88");
+SELECT COUNT(*) AS TotalRecords
+FROM Marksheet;
 
-select * from marksheet where marks>80 order by marks DESC;
+SELECT COUNT(*) AS ArunTest
+FROM Marksheet
+WHERE RollNo = 1
+AND Name = 'Arun'
+AND Department = 'CSE'
+
+SELECT COUNT(*) AS DivyaTest
+FROM Marksheet
+WHERE RollNo = 2
+AND Name = 'Divya'
+AND Department = 'IT'
+AND Marks = 78;
+
+SELECT COUNT(*) AS KarthikTest
+FROM Marksheet
+WHERE RollNo = 3
+AND Name = 'Karthik'
+AND Department = 'CSE'
+AND Marks = 92;
+
+SELECT COUNT(*) AS NishaTest
+FROM Marksheet
+WHERE RollNo = 4
+AND Name = 'Nisha'
+AND Department = 'ECE'
+AND Marks = 67;
+
+SELECT COUNT(*) AS RahulTest
+FROM Marksheet
+WHERE RollNo = 5
+AND Name = 'Rahul'
+AND Department = 'IT'
+AND Marks = 88;
+
+SELECT COUNT(*) AS Above80
+FROM Marksheet
+WHERE Marks > 80;
+
+SELECT RollNo, Name, Department, Marks
+FROM Marksheet
+WHERE Marks > 80
+ORDER BY Marks DESC;
